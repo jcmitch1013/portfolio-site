c9f5bfa597ac9e3f94c29794769e6d5704c755d4
catbath <joshua.mitchell2011@gmail.com>
2022-08-22 16:27:33 -0400

    added questionnaire

---
+++ survey/script.js
@@ -0,0 +1,7 @@
+const element = document.getElementById('myBtn');
+
+element.addEventListener('click', function () {
+  const firstName = document.getElementById('fname').value;
+  cons;
+  console.log(firstName);
+});
---
+++ survey/survey.php
@@ -0,0 +1,16 @@
+    <?php
+if (isset($_POST['submit'])){
+
+
+   $fname = $_POST["fname"];
+    echo "Hello, " . $fname;
+
+    
+
+    mail('')
+
+  }
+
+
+?>
+
